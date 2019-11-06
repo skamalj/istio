ba1d7add13f1ead6f8d22565985676f077603db0
Kamal <skamalj@outlook.com>
2019-11-06 22:12:40 +0530

    Initial commit

---
+++ frontend/controllers/custListController.js
@@ -0,0 +1,29 @@
+
+var populateCustomerList = function(req, res){
+	var request = require('request');
+	request({url:'http://customer-svc:9000/getCustomers',qs:req.query}, function (error, response, body) {
+      if (!error) {
+		res.send(body);
+	  }	
+	  else {
+		  console.log(error)
+	  }
+    });
+}
+
+var populateCustomerDetails = function(req, res){
+	var request = require('request');
+	request('http://customer-detail-svc:9000/getCustomerDetails/' + req.params.custid, function (error, response, body) {
+      if (!error) {
+		res.send(body);
+	  }	
+	  else {
+		  console.log(error)
+	  }
+    });
+}
+
+module.exports = {
+		populateCustomerList: populateCustomerList,
+		populateCustomerDetails: populateCustomerDetails
+}
